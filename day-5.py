1912f4c883da1a8f99a00c62d046179210b4d848
MsMindcracker <indhumani.jajula15@gmail.com>
2025-09-21 18:48:50 +0530

    Add files via upload

---
+++ day-5.py
@@ -0,0 +1,10 @@
+# Mini Project
+# Student Introduction program
+name = input("Name:")
+age = int (input("Age:"))
+college = input("College:")
+course = input("Course:")
+print(f"""
+Student Profile:
+{name},Age{age},pursuing {course} at {college} 
+""" )
